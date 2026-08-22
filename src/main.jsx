2f9d4308454dc03aab5a92df67adae6a874e1174
MaviUK <141088900+MaviUK@users.noreply.github.com>
2026-08-23 00:00:50 +0100

    Load booking comments bridge

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import './bookingScheduleBridge.js';
+import './bookingCommentsBridge.js';
 import './serviceAreaGuard.js';
 import './serviceAreaLinks.js';
 
