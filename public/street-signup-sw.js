ec26fb34e1c85f2a0f869ff4fc4ede4d98d7d1ca
MaviUK <141088900+MaviUK@users.noreply.github.com>
2026-08-24 01:42:17 +0100

    Add Street Signup PWA service worker

---
+++ public/street-signup-sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'ni-bin-guy-street-signup-v1';
+const APP_SHELL = [
+  '/street-booking',
+  '/street-signup.webmanifest',
+  '/street-signup-192.png',
+  '/street-signup-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).catch(() => undefined)
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key.startsWith('ni-bin-guy-street-signup-') && key !== CACHE_NAME)
+        .map((key) => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
